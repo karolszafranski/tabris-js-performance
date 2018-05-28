7c1c9f5682376260d17f4593a4ed83e342b9bb42
Karol Szafrański <kszafranski@eclipsesource.com>
2018-05-28 10:47:21 +0200

    set text instead of creating objects

--- direct-cached/app.js
+++ direct-cached/app.js
@@ -4,16 +4,28 @@ const k = 1000, l = 10;
 const MARGIN = 20;
 
 function test() {
+    const button = new Button();
     var startDate = new Date();
     for (let index = 0; index < k; index++) {
-        new Button();
+        button.text = 'abc';
     }
     tabris.trigger('flush');
     var duration = new Date() - startDate;
     return duration;
 }
 
+var result = 0;
+var results = [];
 for (let run = 0; run < l; run++) {
-    console.log('Run ' + (run + 1) + ': ' + test() + 'ms.');
+    result = test();
+    results.push(result);
+    console.log('Run ' + (run + 1) + ': ' + result + 'ms.');
 }
+
+var sum = 0;
+for (let index = 0; index < results.length; index++) {
+    const element = results[index];
+    sum += element;
+}
+console.log('Average: ' + (sum / results.length));
 console.error('Finished');
